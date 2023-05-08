fa64125381aa1d62575a9b02baf82f543c754e38
wayneliang <1220043823@qq.com>
2023-05-09 00:48:10 +0800

    save

--- src/mixin/statistic-mixin.js
+++ src/mixin/statistic-mixin.js
@@ -2,15 +2,63 @@ import * as Sentry from "@sentry/vue"
 import { mapGetters } from "vuex"
 
 export default {
+  data() {
+    return {
+      startTime: undefined,
+      endTime: undefined
+    }
+  },
   mounted() {
-    Sentry.captureMessage('user-statistic', {
-      user: {
-        username: this.username
+    this.uploadVisitData()
+    this.uploadVisitDuration()
+  },
+  methods: {
+    // 统计PV、UV、IP数、来源、入口页面
+    uploadVisitData() {
+      Sentry.captureMessage("user-statistic", {
+        user: {
+          username: this.username
+        },
+        tags: {
+          referrer: document.referrer || '地址栏直接键入',
+          entry: this.$route.name
+        },
+        level: "info"
+      })
+    },
+    // 统计跳出率和平均访问时长
+    uploadVisitDuration() {
+      this.$once("hook:mounted", () => {
+        this.startTime = performance.now()
+      })
+      this.$once("hook:beforeDestroy", () => {
+        this.endTime = performance.now()
+        Sentry.captureMessage("user-statistic:leave", {
+          tags: {
+            visitDuration: Math.round((this.endTime - this.startTime) / 1000)
+          },
+          level: "info"
+        })
+      })
+    },
+    // 统计受访页面
+    uploadVisitingData() {
+      Sentry.captureMessage('user-statistic:visiting', {
+        tags: {
+          route: this.$route.name
+        }
+      })
+    }
+  },
+  watch: {
+    '$route': {
+      handler() {
+        this.uploadVisitingData()
       },
-      level: 'info'
-    })
+      immediate: true
+    }
   },
   computed: {
-    ...mapGetters({ username: 'name' })
+    ...mapGetters({ username: "name" })
   }
 }
--- src/views/components-demo/markdown.vue
+++ src/views/components-demo/markdown.vue
@@ -1,48 +1,51 @@
 <template>
   <div class="components-container">
-    <aside>Markdown is based on
-      <a href="https://github.com/nhnent/tui.editor" target="_blank">tui.editor</a> ，simply wrapped with Vue.
+    <aside>
+      Markdown is based on
+      <a
+        href="https://github.com/nhnent/tui.editor"
+        target="_blank"
+      >tui.editor</a>
+      ，simply wrapped with Vue.
       <a
         target="_blank"
         href="https://panjiachen.github.io/vue-element-admin-site/feature/component/markdown-editor.html"
       >
-        Documentation </a>
+        Documentation
+      </a>
     </aside>
 
     <div class="editor-container">
-      <el-tag class="tag-title">
-        Basic:
-      </el-tag>
-      <markdown-editor v-model="content1" height="300px" />
+      <el-tag class="tag-title"> Basic: </el-tag>
+      <!-- <markdown-editor v-model="content1" height="300px" /> -->
     </div>
 
     <div class="editor-container">
-      <el-tag class="tag-title">
-        Markdown Mode:
-      </el-tag>
-      <markdown-editor ref="markdownEditor" v-model="content2" :options="{hideModeSwitch:true,previewStyle:'tab'}" height="200px" />
+      <el-tag class="tag-title"> Markdown Mode: </el-tag>
+      <!-- <markdown-editor ref="markdownEditor" v-model="content2" :options="{hideModeSwitch:true,previewStyle:'tab'}" height="200px" /> -->
     </div>
 
     <div class="editor-container">
-      <el-tag class="tag-title">
-        Customize Toolbar:
-      </el-tag>
-      <markdown-editor v-model="content3" :options="{ toolbarItems: ['heading','bold','italic']}" />
+      <el-tag class="tag-title"> Customize Toolbar: </el-tag>
+      <!-- <markdown-editor v-model="content3" :options="{ toolbarItems: ['heading','bold','italic']}" /> -->
     </div>
 
     <div class="editor-container">
-      <el-tag class="tag-title">
-        I18n:
-      </el-tag>
+      <el-tag class="tag-title"> I18n: </el-tag>
       <el-alert
         :closable="false"
         title="You can change the language of the admin system to see the effect"
         type="success"
       />
-      <markdown-editor ref="markdownEditor" v-model="content4" :language="language" height="300px" />
+      <!-- <markdown-editor ref="markdownEditor" v-model="content4" :language="language" height="300px" /> -->
     </div>
 
-    <el-button style="margin-top:80px;" type="primary" icon="el-icon-document" @click="getHtml">
+    <el-button
+      style="margin-top: 80px"
+      type="primary"
+      icon="el-icon-document"
+      @click="getHtml"
+    >
       Get HTML
     </el-button>
     <div v-html="html" />
@@ -50,7 +53,7 @@
 </template>
 
 <script>
-import MarkdownEditor from '@/components/MarkdownEditor'
+// import MarkdownEditor from '@/components/MarkdownEditor'
 
 const content = `
 **This is test**
@@ -61,25 +64,25 @@ const content = `
 
 `
 export default {
-  name: 'MarkdownDemo',
-  components: { MarkdownEditor },
+  name: "MarkdownDemo",
+  // components: { MarkdownEditor },
   data() {
     return {
       content1: content,
       content2: content,
       content3: content,
       content4: content,
-      html: '',
+      html: "",
       languageTypeList: {
-        'en': 'en_US',
-        'zh': 'zh_CN',
-        'es': 'es_ES'
+        en: "en_US",
+        zh: "zh_CN",
+        es: "es_ES"
       }
     }
   },
   computed: {
     language() {
-      return this.languageTypeList['en']
+      return this.languageTypeList["en"]
     }
   },
   methods: {
@@ -92,10 +95,10 @@ export default {
 </script>
 
 <style scoped>
-.editor-container{
+.editor-container {
   margin-bottom: 30px;
 }
-.tag-title{
+.tag-title {
   margin-bottom: 5px;
 }
 </style>
